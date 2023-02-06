ea371e425440c2ec8de0ac10454e0efae6d06d10
pamtel <pamelaakosahiante@gmail.com>
2023-02-06 16:27:13 +0100

    setup native environment

---
+++ constants/icons.ts
@@ -0,0 +1,23 @@
+const back_arrow: Object = require('../assets/icons/back_arrow.png');
+const home: Object = require('../assets/icons/home.png');
+const line_graph: Object = require('../assets/icons/line_graph.png');
+const notification_color: Object = require('../assets/icons/notification_color.png');
+const notification_white: Object = require('../assets/icons/notification_white.png');
+const pie_chart: Object = require('../assets/icons/pie_chart.png');
+const right_arrow: Object = require('../assets/icons/right_arrow.png');
+const settings: Object = require('../assets/icons/settings.png');
+const star: Object = require('../assets/icons/star.png');
+const transaction: Object = require('../assets/icons/transaction.png');
+
+export default {
+  back_arrow,
+  home,
+  line_graph,
+  notification_color,
+  notification_white,
+  pie_chart,
+  right_arrow,
+  settings,
+  star,
+  transaction,
+};
---
+++ constants/images.ts
@@ -0,0 +1,13 @@
+const banner: Object = require('../assets/images/banner.png');
+const bitcoin: Object = require('../assets/images/bitcoin.png');
+const ethereum: Object = require('../assets/images/ethereum.png');
+const litecoin: Object = require('../assets/images/litecoin.png');
+const ripple: Object = require('../assets/images/ripple.png');
+
+export default {
+  banner,
+  bitcoin,
+  ethereum,
+  litecoin,
+  ripple,
+};
